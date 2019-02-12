cedf4290c62317b1c01d6fe4c83a2c2af2477c29
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-12 15:00:26 +0100

    Add login mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -25,7 +25,17 @@ const mutation = new GraphQLObjectType({
         req.logout();
         return user;
       }
-    }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
+    },
   }
 });
 module.exports = mutation;
