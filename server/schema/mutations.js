cd113bed5eae839891e4f87e4d4490d6f98136c3
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-12 14:59:01 +0100

    Add mutation to logout

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -3,6 +3,7 @@ const graphql = require("graphql");
 const { GraphQLString, GraphQLObjectType } = graphql;
 
 const UserType = require("./types/user_type");
+const AuthService = require("../services/auth");
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -13,8 +14,16 @@ const mutation = new GraphQLObjectType({
         email: { type: GraphQLString },
         password: { type: GraphQLString }
       },
-      resolve(parentValue, args, request) {
-
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.signup({ email, password, req });
+      }
+    },
+    logout: {
+      type: UserType,
+      resolve(parentValue, args, req) {
+        const { user } = req;
+        req.logout();
+        return user;
       }
     }
   }
