1b9525e5f52d2064b7adbd6ee0fc62e600867521
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-12 18:52:46 +0100

    Fetch user

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { Router, Route, hashHistory } from "react-router";
 
-import App from './components/App';
+import App from "./components/App";
 
 const client = new ApolloClient({
   dataIdFromObject: o => o.id
@@ -14,7 +14,7 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path="/" component={App}/>
+        <Route path="/" component={App} />
       </Router>
     </ApolloProvider>
   );
