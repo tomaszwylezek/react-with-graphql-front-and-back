05373bf312b3183ffec61e6da3f8322543014fb1
Tomasz Wylężek <tomwylezek@gmail.com>
2019-02-13 18:19:18 +0100

    Add AuthForm

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
+import AuthForm from "./AuthForm";
+
 class LoginForm extends Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div>
+        <AuthForm />
+      </div>
+    );
   }
 }
 
